df68bf3abc75e571ce5301459fced9e56847bcb2
Ruan <ruanfiorimarcelino@gmail.com>
2024-06-19 15:08:14 -0300

    firebase correct

--- Snackup/src/config/firebaseconfig.js
+++ Snackup/src/config/firebaseconfig.js
@@ -10,18 +10,16 @@ import {
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAgI44ssvRoJz4bJtmqhEr1F2rDWEZ6LoQ",
-  authDomain: "task-912c4.firebaseapp.com",
-  projectId: "task-912c4",
-  storageBucket: "task-912c4.appspot.com",
-  messagingSenderId: "726424803847",
-  appId: "1:726424803847:web:49e850d55029dcc646e8a2",
+  apiKey: "AIzaSyCARgnJtbsZ0n4onUUDxmvTyF5rLbDboeA",
+  authDomain: "snackup-70c0e.firebaseapp.com",
+  projectId: "snackup-70c0e",
+  storageBucket: "snackup-70c0e.appspot.com",
+  messagingSenderId: "522247615888",
+  appId: "1:522247615888:web:33c7999d9627beb5afef15",
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const database = getFirestore(app);
-
 const auth = getAuth(app);
 
 export {
